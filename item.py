5a34b7ee62936095a96018937ed053008d92f419
xiaoming.su <xiaoming.su@iyoyobuy.com>
2020-01-29 10:40:06 +0800

    items

---
+++ item.py
@@ -0,0 +1,33 @@
+# @see https://shimo.im/docs/tcXtdyK9cVhP8cRh/read
+
+class Item:
+    def __init__(self, name, keywords=None):
+        self.name = name
+        self.keywords = [name] if not keywords else keywords
+        self.keywords = set(self.keywords + [name])
+
+    def __repr__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+
+# 口罩类
+N95 = Item('N95口罩', ['N95防护口罩', '防护口罩'])
+医用外科口罩 = Item('医用外科口罩', ['外科口罩'])
+一次性医用口罩 = Item('一次性医用口罩', ['一次性口罩'])
+# 防护类
+防护服 = Item('防护服')
+一次性医用帽 = Item('一次性医用帽', ['一次性工作帽'])
+护目镜 = Item('护目镜', ['防护眼镜'])
+防冲击眼罩 = Item('防冲击眼罩')
+防护面罩 = Item('防护面罩', ['防护面屏'])
+医用一次性乳胶手套 = Item('医用一次性乳胶手套', ['一次性乳胶手套', '一次性医用手套'])
+一次性手术衣 = Item('一次性手术衣')
+# 消毒类
+消毒液 = Item('84消毒液')
+过氧化氢消毒液 = Item('过氧化氢消毒液')
+医用酒精 = Item('医用酒精', ['医用消毒酒精'])
+
+AllItems = [N95, 医用外科口罩, 一次性医用口罩, 防护服, 一次性医用帽, 护目镜, 防冲击眼罩, 防护面罩, 医用一次性乳胶手套, 一次性手术衣, 消毒液, 医用酒精]
---
+++ parser.py
@@ -0,0 +1,52 @@
+from bs4 import BeautifulSoup
+
+
+class NoticeParser:
+    def __init__(self, html):
+        self.html = html
+
+    def parse(self):
+        page = BeautifulSoup(self.html)
+        title = page.title.string
+
+        notice_body = None
+        for div in page.find_all('div'):
+            text = div.get_text()
+            if '版权所有' in text:
+                continue
+            if '物资' not in text:
+                continue
+            for kw in ('新型', '肺炎', '防疫', '防控', '疫情'):
+                if kw in text:
+                    break
+            notice_body = div
+        if not notice_body:
+            print('Failed to extra notice info...', title)
+            return
+        notice = notice_body.get_text().upper()
+        requirements = self.extract_requirements(notice)
+        print(notice, requirements)
+
+    # 所需物资
+    def extract_requirements(self, notice):
+        from item import AllItems
+
+        result = []
+        for item in AllItems:
+            found = False
+            for keyword in item.keywords:
+                if keyword in notice:
+                    found = True
+                    break
+            if found:
+                result.append(item)
+        return result
+
+
+if __name__ == '__main__':
+    import glob
+
+    for file in glob.glob('data/notices/*.html'):
+        html = open(file, encoding='utf8').read()
+        p = NoticeParser(html)
+        p.parse()
