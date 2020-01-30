8141fea44457843c976350e36b0a66a5e0440622
PyBeaner <pybeaner@qq.com>
2020-01-30 15:20:50 +0800

    fix location

--- extractors/location.py
+++ extractors/location.py
@@ -9,14 +9,16 @@ class LocationExtractor(Extractor):
     remove_spaces = False
 
     def extract(self):
-        p = re.compile(r'捐?赠?地[址点]：?:?([\w\d()（）、，-]*)')
+        p = re.compile(r'捐?赠?地[址点]：?:?([\w\d()（）、，-]+)')
         result = ''
         for node in self.page.find_all(re.compile(r'span|div|p')):
             text = node.text.strip().replace(' ', '').replace(' ', '')
             if '地址' in text or '地点' in text:
-                tmp = p.findall(text)[0]
-                if len(tmp) < 100:
-                    result = tmp
+                tmp = p.findall(text)
+                if not tmp:
+                    continue
+                if len(tmp[0]) < 100:
+                    result = tmp[0]
         result = result.strip().strip('。')
         return result
 
@@ -24,6 +26,6 @@ class LocationExtractor(Extractor):
 if __name__ == '__main__':
     # page = get_demo()
     html = BeautifulSoup(
-        '''<p style="text-indent: 2em; text-align: left">地&nbsp;&nbsp;址：霸州市金康东道228号（霸州市卫健局325房间）</p>''')
+        '''<p style="text-indent:43px;text-autospace:ideograph-numeric;line-height:36px"><span style=";font-family:宋体;font-size:21px"><span style="font-family:宋体">地址：</span></span><span style=";font-family:宋体;font-size:21px"><span style="font-family:宋体">文圣区衍水大街</span>38<span style="font-family:宋体">号（区便民服务大厅民政窗口）</span></span></p>''')
     e = LocationExtractor(html.text, html)
     print(e.extract())
